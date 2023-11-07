5136dbb4873bae8b0c9cbf1d2d54b772f2ce1fe5
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-11-07 18:22:31 -0300

    initial settings

---
+++ landing-page/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarComponent } from './components/car/car.component';
+import { RequestQuoteComponent } from './components/request-quote/request-quote.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    CarComponent,
+    RequestQuoteComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ landing-page/src/app/components/request-quote/request-quote.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-quote',
+  templateUrl: './request-quote.component.html',
+  styleUrls: ['./request-quote.component.css']
+})
+export class RequestQuoteComponent {
+
+}
